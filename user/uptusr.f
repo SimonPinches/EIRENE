f7c8ba8306b1f10f79b2027e5abcaca1dde9c384
Petra Boerner <p.boerner@fz-juelich.de>
2014-04-15 16:57:27 +0200

    added for B2.5 coupling

--- user/uptusr.f
+++ user/uptusr.f
@@ -29,6 +29,8 @@ CDR
 CDR
       INTEGER :: IAT, IPL, I, IR, IP, IRD
       INTEGER, SAVE :: IFIRST, IA1, IA2, IA3, NA4, INDEXM, INDEXF
+      integer :: icou
+      real(dp) :: wtr,vr,vp,dist
       DATA IFIRST/0/
  
       IF (IFIRST.EQ.0) THEN
@@ -72,6 +74,69 @@ C
         NA4=4*IA1
         INDEXM=NPLSI
         INDEXF=2*NPLSI
+      ENDIF
+csw
+csw added for B2.5 coupling (ank_mods in eirene_mc.F of B2.5)
+csw so called 'STANDARD' option in Vlad's SOLPS4.3 user/uptusr.f
+csw 21oct2011
+csw
+C
+C  WV=WEIGHT/VEL
+C
+C  ATOMS
+      IF (ITYP.EQ.1) THEN
+        DO 20 ICOU=1,NCOU
+          DIST=CLPD(ICOU)
+          WTR=WV*DIST
+          IRD=NRCELL+NUPC(ICOU)*NR1ST+NBLCKA
+C
+          IF (LGVAC(IRD,0)) GOTO 20
+C
+CDR
+C  RADIAL GESCHWINDIGKEITSKOMPONENTE  (CM/SEC)
+          VR=(VELX*VPX(IRD)+VELY*VPY(IRD))*VEL
+          if(iatm.gt.nadv) goto 20
+          ADDV(IATM,IRD)=ADDV(IATM,IRD)+WTR*VR
+          if(ia1+iatm.gt.nadv) goto 20
+          ADDV(IA1+IATM,IRD)=ADDV(IA1+IATM,IRD)+WTR*VR*E0
+C  POLOIDALE GESCHWINDIGKEITSKOMPONENTE (CM/SEC)
+          VP=(VELX*VRX(IRD)+VELY*VRY(IRD))*VEL
+          if(ia2+iatm.gt.nadv) goto 20
+          ADDV(IA2+IATM,IRD)=ADDV(IA2+IATM,IRD)+WTR*VP
+          if(ia3+iatm.gt.nadv) goto 20
+          ADDV(IA3+IATM,IRD)=ADDV(IA3+IATM,IRD)+WTR*VP*E0
+CDR
+20      CONTINUE
+C
+C  MOLECULES
+      ELSEIF (ITYP.EQ.2) THEN
+CDR
+        DO 200 ICOU=1,NCOU
+          DIST=CLPD(ICOU)
+          WTR=WV*DIST
+          IRD=NRCELL+NUPC(ICOU)*NR1ST+NBLCKA
+C
+          IF (LGVAC(IRD,0)) GOTO 200
+C  RADIAL GESCHWINDIGKEITSKOMPONENTE (CM/SEC)
+          VR=(VELX*VPX(IRD)+VELY*VPY(IRD))*VEL
+          if(natmi+imol.gt.nadv) goto 200
+          ADDV(NATMI+IMOL,IRD)=ADDV(NATMI+IMOL,IRD)+WTR*VR
+          if(ia1+natmi+imol.gt.nadv) goto 200
+          ADDV(IA1+NATMI+IMOL,IRD)=ADDV(IA1+NATMI+IMOL,IRD)+WTR*VR*E0
+C  POLOIDALE GESCHWINDIGKEITSKOMPONENTE (CM/SEC)
+          VP=(VELX*VRX(IRD)+VELY*VRY(IRD))*VEL
+          if(ia2+natmi+imol.gt.nadv) goto 200
+          ADDV(IA2+NATMI+IMOL,IRD)=ADDV(IA2+NATMI+IMOL,IRD)+WTR*VP
+          if(ia3+natmi+imol.gt.nadv) goto 200
+          ADDV(IA3+NATMI+IMOL,IRD)=ADDV(IA3+NATMI+IMOL,IRD)+WTR*VP*E0
+C
+200     CONTINUE
+CDR
+C
+C  TEST IONS
+      ELSEIF (ITYP.EQ.3) THEN
+C  TO BE WRITTEN
+C
       ENDIF
       RETURN
       END
