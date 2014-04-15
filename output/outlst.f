dae9e48df1b5e398b5632d54ccda10a3ed1ec42f
Petra Boerner <p.boerner@fz-juelich.de>
2014-04-15 16:51:50 +0200

    output improved

--- output/outlst.f
+++ output/outlst.f
@@ -1,4 +1,11 @@
       SUBROUTINE EIRENE_OUTLST
+
+C  DIAGNOSTIC OUTPUT AFTER LAST HISTORY, BUT STILL INSIDE PARTICLE LOOP 
+
+C  MOSTLY: CPU EFFICIANCY ISSUES
+
+
+
  
       USE EIRMOD_PRECISION
       USE EIRMOD_PARMMOD
@@ -11,7 +18,8 @@
  
       REAL(DP) :: SMMEAN
       INTEGER :: IWR, IRCX, IREL, IRPI
- 
+
+C  1) EFFICIANCY OF RECECTION SAMPLING IN VELOCX, VELOEL, VELOPI 
       call EIRENE_leer(1)
       iwr=0
       do ircx=1,nrcxi
@@ -19,11 +27,12 @@
           if (iwr.eq.0) then
             WRITE (iunout,*) 'REJECTION SAMPLING EFFICIENCY IN VELOCX '
             write (iunout,*)
-     .        'IRCX, MEAN NO. OF SAMPLING, TOTAL NO. OF CALLSEIRENE_'
+     .      'IRCX, TOTAL NO. OF CALLS TO VELOCX, MEAN NO. OF SAMPLING'
             iwr=1
           endif
           SMMEAN=xcmean(ircx)/(ncmean(ircx)+eps60)
-          write (iunout,*) ircx,SMMEAN,NCMEAN(IRCX)
+          CALL EIRENE_MASJ2R('IRCX, NCMEAN, SMMEAN    ',
+     .                        IRCX, NCMEAN(IRCX),SMMEAN) 
         endif
       enddo
       call EIRENE_leer(1)
@@ -33,11 +42,12 @@
           if (iwr.eq.0) then
             WRITE (iunout,*) 'REJECTION SAMPLING EFFICIENCY IN VELOEL '
             write (iunout,*)
-     .        'IREL, MEAN NO. OF SAMPLING, TOTAL NO. OF CALLSEIRENE_'
+     .      'IREL, TOTAL NO. OF CALLS TO VELOEL, MEAN NO. OF SAMPLING'
             iwr=1
           endif
           SMMEAN=xemean(irel)/(nemean(irel)+eps60)
-          write (iunout,*) irel,SMMEAN,NEMEAN(IREL)
+          CALL EIRENE_MASJ2R('IREL, NEMEAN, SMMEAN    ',
+     .                        IREL, NEMEAN(IREL),SMMEAN) 
         endif
       enddo
       call EIRENE_leer(1)
@@ -47,11 +57,12 @@
           if (iwr.eq.0) then
             WRITE (iunout,*) 'REJECTION SAMPLING EFFICIENCY IN VELOPI '
             write (iunout,*)
-     .        'IRPI, MEAN NO. OF SAMPLING, TOTAL NO. OF CALLSEIRENE_'
+     .      'IRPI, TOTAL NO. OF CALLS TO VELOPI, MEAN NO. OF SAMPLING'
             iwr=1
           endif
           SMMEAN=xpmean(irpi)/(npmean(irpi)+eps60)
-          write (iunout,*) irpi,SMMEAN,NPMEAN(IRPI)
+          CALL EIRENE_MASJ2R('IRPI, NPMEAN, SMMEAN    ',
+     .                        IRPI, NPMEAN(IRPI),SMMEAN) 
         endif
       enddo
       call EIRENE_leer(1)
