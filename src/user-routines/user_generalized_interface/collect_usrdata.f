883b3c76c613eb5caa817d91124f997a6500dc27
Petra Boerner <p.boerner@fz-juelich.de>
2015-05-27 14:17:04 +0200

    optional argument ICALL added to subroutine EIRENE_COLLECT_USRDATA

--- src/user-routines/user_generalized_interface/collect_usrdata.f
+++ src/user-routines/user_generalized_interface/collect_usrdata.f
@@ -1,6 +1,9 @@
 
 
-      SUBROUTINE EIRENE_COLLECT_USRDATA
+      SUBROUTINE EIRENE_COLLECT_USRDATA( ICALL )
       IMPLICIT NONE
+C> This flag allows to detect the second call of this subroutine.
+C> Allowed value: 2.
+      INTEGER, OPTIONAL, INTENT(IN) :: ICALL
       RETURN
       END
--- src/user-routines/user_iter/collect_usrdata.f
+++ src/user-routines/user_iter/collect_usrdata.f
@@ -1,6 +1,9 @@
 
 
-      SUBROUTINE EIRENE_COLLECT_USRDATA
+      SUBROUTINE EIRENE_COLLECT_USRDATA(icall)
       IMPLICIT NONE
+C> This flag allows to detect the second call of this subroutine.
+C> Allowed value: 2.
+      integer, optional, intent(in) :: icall
       RETURN
       END
