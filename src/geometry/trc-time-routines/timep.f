872bd91cdce37a9c958782052ded43e492b59468
D.Reiter <d.reiter@fz-juelich.de>
2016-10-04 10:24:35 +0200

    bug fix in timep.f: in 2D, 3D levgeo=1 option. Remove error exit due to round off errors. This unnecessary safety measure had led to occasional code failures in very long runs (very rare events), although the code seems to be able to cope with these rare rounding errors ("failed operational mode") without going to "failed save mode".

--- src/geometry/trc-time-routines/timep.f
+++ src/geometry/trc-time-routines/timep.f
@@ -1,3 +1,6 @@
+cdr  oct 16:  levgeo=1, error exit due to round off errors removed,
+cdr                     now: "save landing of lost particle".
+cdr           prevents random error exits in very long (parallel) runs.
 C
 C
       SUBROUTINE EIRENE_TIMEP (ZRAD)
@@ -266,7 +269,7 @@ C
           else
              itc = ncell
           end if
-!pb          IF (ITEST.NE.0.AND.ILIIN(IN).NE.0) THEN
+!pb                              IF (ITEST.NE.0.AND.ILIIN(IN).NE.0) THEN
           IF (.not.ldamcel(itc).and.(ITEST.NE.0.AND.ILIIN(IN).NE.0))THEN
 C
 C  TRACK ENDS ON ONE OF THE NON DEFAULT POLOIDAL SURFACES
@@ -774,7 +777,8 @@ C
 3000    CONTINUE
         IF (J2.LE.0.OR.J2.GT.NP2ND) THEN
           WRITE (iunout,*) 'ERROR IN TIMEP ',J2,J1,VELY
-          CALL EIRENE_EXIT_OWN(1)
+          RETURN  ! changed to avoid job crash in long runs
+cdr       CALL EIRENE_EXIT_OWN(1)
         ENDIF
 C  TIME FROM Y00 TO PSURF
         IF (MPSURF.EQ.J2) THEN
