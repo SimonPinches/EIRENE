4d2d90ef4c330675be19330fa5b9fd5fca410933
Michael Rack <m.rack@fz-juelich.de>
2017-06-01 21:18:27 +0200

    Correction in gr3net dummy.

--- src/grsoft-dummy/gr3net.f
+++ src/grsoft-dummy/gr3net.f
@@ -2,8 +2,7 @@ c------------------------------------------------------------------------
       SUBROUTINE GR3NET(AR,IER,I1,XYZ,I2,I3,I4,I5,I6,I7)
       IMPLICIT NONE
       REAL, INTENT(OUT) :: AR(*)
-      REAL, INTENT(IN) :: XYZ(3,*,*)
+      REAL, INTENT(IN) :: XYZ(*)
       INTEGER, INTENT(IN) :: IER, I1, I2, I3, I4, I5, I6, I7
-      AR = 0
       RETURN
       END
