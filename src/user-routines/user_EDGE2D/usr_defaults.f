80423426c411b32089a16cf41c2848aacbc81ff7
Derek Harting <dharting@jet.uk>
2017-08-07 17:26:42 +0100

    Reset IUNIN=1 in USR_DEFAULTS to avoid accumulation of IFOFF

--- src/user-routines/user_EDGE2D/usr_defaults.f
+++ src/user-routines/user_EDGE2D/usr_defaults.f
@@ -2,9 +2,12 @@
 
       use eirmod_precision
       use eirmod_parmmod
+      use eirmod_comprt,only: IUNIN
 
       implicit none
 
+c     Reset IUNIN to 1
+      IUNIN = 1
 c     set offset for file units
       IFOFF = 500
       
