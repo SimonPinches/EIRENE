f451debf621feb8a049b9609328ed5b3080a5d1c
Petra Boerner <p.boerner@fz-juelich.de>
2017-10-17 09:48:43 +0200

    allow for additional cell in triangular meshes

--- src/startup-routines/grid.f
+++ src/startup-routines/grid.f
@@ -506,9 +506,11 @@ C  INDEX IS:           TRIANGLE SIDE
             J = INMTI(IS,IT)
             IF ( J .NE. 0) THEN
               IF ((NCHBAR(IS,IT) == 0) .AND. (ILIIN(J) <= 0)) THEN
-                WRITE (iunout,*) 'SIDE',IS,' OF TRIANGLE ',IT,
-     .              ' IS TRANSPARENT BUT HAS NO NEIGHBOR '
-                LERROR = .TRUE.
+                IF ((ILIIN(J) < 0) .AND. (ILSWCH(J) < 1000)) THEN
+                  WRITE (iunout,*) 'SIDE',IS,' OF TRIANGLE ',IT,
+     .               ' IS TRANSPARENT BUT HAS NO NEIGHBOR '
+                  LERROR = .TRUE.
+                END IF
               END IF
               SURF_TRIAN(J)%NUMTR = SURF_TRIAN(J)%NUMTR + 1
               SURF_TRIAN(J)%ITRIAS(SURF_TRIAN(J)%NUMTR) = IT
