9e64e7b593a6178e285d44b446ec3187bfa54f99
Petra Boerner <p.boerner@fz-juelich.de>
2018-03-22 11:45:11 +0100

    score cpu time species dependent

--- src/particle-tracing/switch_partinfo.f
+++ src/particle-tracing/switch_partinfo.f
@@ -21,6 +21,10 @@ c  Output: ixspz,nmetoff,logphot,logatm,logmol,logion
       USE EIRMOD_COMXS
       USE EIRMOD_CZT1
       USE EIRMOD_CSPEZ
+      USE EIRMOD_CTRCEI
+      USE EIRMOD_COMSOU
+      USE EIRMOD_CPES
+      USE EIRMOD_MPI
        
       implicit none
 
@@ -31,11 +35,51 @@ c  Output: ixspz,nmetoff,logphot,logatm,logmol,logion
      .                 imol_old=-1, 
      .                 iion_old=-1,
      .                 ipls_old=-1
+      real(dp), allocatable, save :: time_array(:,:,:), helpt(:,:,:)
+      real(dp) :: tim_spent
+      real(dp), save :: tim_start=0._dp, tim_end=0._dp 
+      real(dp) :: eirene_second_own
+      integer :: istr, nn, ier, itp, isp
       
       if ((ityp_old == ityp) .and. (iatm_old == iatm) .and.
      .    (imol_old == imol) .and. (iion_old == iion) .and.
      .    (iphot_old == iphot) .and. (ipls_old == ipls).and.
      .    (istra_old == istra)) return
+      
+      if (trchktim) then
+        tim_end = EIRENE_SECOND_OWN()
+        tim_spent = tim_end - tim_start
+        tim_start = tim_end
+
+        select case(ityp_old)
+        case(0)
+!  photons: not ready
+          time_array(ityp_old,iphot_old,istra_old) = 
+     .      time_array(ityp_old,iphot_old,istra_old) + tim_spent
+        case(1)
+!  atoms
+          time_array(ityp_old,iatm_old,istra_old) = 
+     .      time_array(ityp_old,iatm_old,istra_old) + tim_spent
+        case(2)
+!  molecules
+          time_array(ityp_old,imol_old,istra_old) = 
+     .      time_array(ityp_old,imol_old,istra_old) + tim_spent
+        case(3)
+!  test ions
+          time_array(ityp_old,iion_old,istra_old) = 
+     .      time_array(ityp_old,iion_old,istra_old) + tim_spent
+        case(4)
+!  bulk ions
+!          time_array(ityp_old,ipls_old,istra_old) = 
+!     .      time_array(ityp_old,ipls_old,istra_old) + tim_spent
+        case default
+          if (.not.allocated(time_array)) then
+            allocate (time_array(0:3,
+     .                     0:max(nphot,natm,nmol,nion),0:nstra))
+            time_array = 0._dp
+          end if
+        end select
+      end if
 
 C  save stratum, old type, species
       istra_old= istra
@@ -319,5 +363,113 @@ cdr  as long as update, collide, fpath for photons are still kept as separate ro
       end select 
 
       return
+
+      entry eirene_output_partinfo
+
+      if (nprs > 0) then
+        nn = max(nphot,natm,nmol,nion)
+        allocate (helpt(0:3,0:nn,0:nstra))
+        helpt = 0._dp
+        call mpi_reduce(time_array,helpt,4*(nn+1)*(nstra+1),
+     .       mpi_double_precision,mpi_sum,0,mpi_comm_world,ier)
+        time_array = helpt
+        deallocate (helpt)
+        if (my_pe /= 0) return
+      end if
+
+      call eirene_leer(2)
+
+      call eirene_headng ('STATISTICS OVER CPU TIME SPENT'//
+     .                    ' IN FOLLOWING TRAJECTORIES ',57)
+
+! sum over species
+      do istr = 1, nstrai
+        time_array(0,0,istr) = sum(time_array(0,1:,istr))
+        time_array(1,0,istr) = sum(time_array(1,1:,istr))
+        time_array(2,0,istr) = sum(time_array(2,1:,istr))
+        time_array(3,0,istr) = sum(time_array(3,1:,istr))
+      end do
+! sum over strata
+      do itp = 0,3
+        do isp = 0,nn
+          time_array(itp,isp,0) = sum(time_array(itp,isp,1:nstrai))
+        end do
+      end do  
+
+      do istr = 0, nstrai
+
+        call eirene_leer(1)
+        if (istr == 0) then
+          write (iunout,'(1x,A,I6)') 'SUM OVER STRATA '
+          write (iunout,'(1x,A,I6)') '=============== '
+        else
+          write (iunout,'(1x,A,I6)') 'STRATUM ',istr
+          write (iunout,'(1x,A,I6)') '============== '
+        end if
+
+        if (any(time_array(0,:,istr) > 0._dp)) then
+          call eirene_leer(1)
+          write (iunout,*) 'TIME (SEC) SPENT IN FOLLOWING '
+          CALL EIRENE_MASYR1 ('PHOTONS = ',time_array(0,0:nphot,:),
+     .                LOGPHOT,ISTR,0,NPHOT,0,NSTRA,TEXTS(1))
+          CALL EIRENE_MASAGE
+     .      ('SUM OVER SPECIES                               ')
+          CALL EIRENE_MASR1 ('TOTAL=  ',SUM(time_array(0,1:nphot,ISTR)))
+        end if
+
+        if (any(time_array(1,:,istr) > 0._dp)) then
+          call eirene_leer(1)
+          write (iunout,*) 'TIME (SEC) SPENT IN FOLLOWING '
+          CALL EIRENE_MASYR1 ('ATOMS =   ',time_array(1,0:natm,:),
+     .                LOGATM,ISTR,0,NATM,0,NSTRA,TEXTS(NSPH+1))
+         CALL EIRENE_MASAGE
+     .      ('SUM OVER SPECIES                               ')
+          CALL EIRENE_MASR1 ('TOTAL=  ',SUM(time_array(1,1:natm,ISTR)))
+        end if
+
+        if (any(time_array(2,:,istr) > 0._dp)) then
+          call eirene_leer(1)
+          write (iunout,*) 'TIME (SEC) SPENT IN FOLLOWING '
+          CALL EIRENE_MASYR1 ('MOLECULES=',time_array(2,0:nmol,:),
+     .                LOGMOL,ISTR,0,NMOL,0,NSTRA,TEXTS(NSPA+1))
+          CALL EIRENE_MASAGE
+     .      ('SUM OVER SPECIES                               ')
+          CALL EIRENE_MASR1 ('TOTAL=  ',SUM(time_array(2,1:nmol,ISTR)))
+        end if
+
+        if (any(time_array(3,:,istr) > 0._dp)) then
+          call eirene_leer(1)
+          write (iunout,*) 'TIME (SEC) SPENT IN FOLLOWING '
+          CALL EIRENE_MASYR1 ('TEST IONS=',time_array(3,0:nion,:),
+     .                LOGION,ISTR,0,NION,0,NSTRA,TEXTS(NSPAM+1))
+          CALL EIRENE_MASAGE
+     .      ('SUM OVER SPECIES                               ')
+          CALL EIRENE_MASR1 ('TOTAL=  ',SUM(time_array(3,1:nion,ISTR)))
+        end if
+
+!        if (any(time_array(4,:,:) > 0._dp)) then
+!          write (iunout,*) ' TIME (SEC) SPENT IN FOLLOWING '
+!          CALL EIRENE_MASYR1 ('BULK IONS=',time_array(4,1:npls,:),
+!     .                LOGPLS,ISTR,1,NPLS,1,NSTRA,TEXTS(NSPAMI+1))
+!          CALL EIRENE_MASAGE
+!     .      ('SUM OVER SPECIES                               ')
+!          CALL EIRENE_MASR1 ('TOTAL=  ',SUM(time_array(4,1:npls,ISTR)))
+!        end if
+
+      end do
+      return
+
+      entry eirene_reinit_partinfo
+
+      if (allocated(time_array)) deallocate(time_array)
+      istra_old=-1
+      ityp_old=-1
+      iphot_old=-1 
+      iatm_old=-1 
+      imol_old=-1 
+      iion_old=-1
+      ipls_old=-1
+
+      return      
       end subroutine eirene_switch_partinfo
 
