719818542ca7d8a657aa8b7f1eb1908faa2b8a6b
D.Reiter <d.reiter@fz-juelich.de>
2018-04-19 17:15:50 +0200

    comment only

--- src/startup-routines/input.f
+++ src/startup-routines/input.f
@@ -110,10 +110,11 @@ C
       USE EIRMOD_CESTIM
       USE EIRMOD_CUPD
       USE EIRMOD_PHOTON
+      USE EIRMOD_MPI
 
       IMPLICIT NONE
 
-      INCLUDE 'mpif.h'
+!      INCLUDE 'mpif.h'
 C
       TYPE TEMPERATURE
         DOUBLE PRECISION          :: TE, TI
@@ -2457,7 +2458,8 @@ c  next: read species index sampling distributions datm, dmol, dion, dpls, and i
       READ (IUNIN,6664) (DMLD(IMOL),IMOL=1,NMOLI_IN)
       READ (IUNIN,6664) (DIOD(IION),IION=1,NIONI_IN)
       READ (IUNIN,6664) (DPLD(IPLS),IPLS=1,NPLSI_IN)
-      IF (NPHOTI > 0)
+      IF (NPHOTI > 0)   !dr try to make this more logic: always read dphd.
+cdr                     !dr backward compatible ?
      .  READ (IUNIN,6664) (DPHD(IPHOT),IPHOT=1,NPHOTI_IN)
 
 c  next: read universal surface reflection model flags
@@ -2863,11 +2865,11 @@ C
 900   CONTINUE
 C
       IF (IREAD.EQ.0) READ (IUNIN,*)
-      IREAD=0
       CALL EIRENE_MASAGE
      .  ('*** 9. DATA FOR STATISTIC AND NON-ANALOG MODEL   ')
 C
-910   READ (IUNIN,'(A72)') ZEILE
+910   IF (IREAD.EQ.0) READ (IUNIN,'(A72)') ZEILE
+      IREAD = 0
       IF (ZEILE(1:1) .EQ. '*') GOTO 910
 C  DATA FOR CONDITIONAL EXPECTATION ESTIMATOR
       NLOGIN = MAX(1,NATMI_IN + NMOLI_IN + NIONI_IN + NPHOTI_IN)
@@ -3051,7 +3053,6 @@ C
           IF (NSMSTRA > 0) ALLOCATE(SMESTL(NADSPC))
         ELSE
           ALLOCATE(ESTIML(1))
-          NULLIFY (ESTIML(1)%PSPC)
         END IF
 C
         DO J=1,NADSPC
@@ -3163,7 +3164,8 @@ C    .      ... WRONG INPUT !
             WRITE (IUNOUT,*) ' SPECTRUM TYPE = ',ISPTYP
           END IF
 
-          ALLOCATE(ESPEC)
+          ESPEC => ESTIML(J)
+          
           ESPEC%ISPCSRF = ISPSRF
           ESPEC%IPRTYP = IPTYP
           ESPEC%IPRSP = IPSPZ
@@ -3218,7 +3220,7 @@ c  standard deviation of spectrally resolved tallies
 
           IF (NSMSTRA > 0) THEN
 c  sum over strata
-            ALLOCATE(SSPEC)
+            SSPEC => SMESTL(J)
             ALLOCATE(SSPEC%SPC(0:NSPSA+1))
 c  standard deviation of spectra tallies, sum over strata intermediate storage
 !           IF (NSIGI_SPC > 0) THEN
@@ -3227,11 +3229,9 @@ c  standard deviation of spectra tallies, sum over strata intermediate storage
               ALLOCATE(SSPEC%STV(0:NSPSA+1))
               ALLOCATE(SSPEC%GG(0:NSPSA+1))
 !           END IF
-            SSPEC = ESPEC
-            SMESTL(J)%PSPC => SSPEC
+            SMESTL(J) = ESTIML(J)
           END IF
 C
-          ESTIML(J)%PSPC => ESPEC
         END DO
         IREAD=0
       ELSE
@@ -3239,7 +3239,6 @@ Cdr  No block 10F found, i.e. no spectrally resolved tallies at all.
 cdr  Why do we allocate estiml in input.f and not in eirmod_cestim ?
         IF (.NOT.ALLOCATED(ESTIML)) THEN
           ALLOCATE(ESTIML(1))
-          NULLIFY (ESTIML(1)%PSPC)
         END IF
       END IF
 C
@@ -3262,7 +3261,7 @@ c  search for input block 11a
      .                  TRCBLPH,TRCTAL,TRCOCT,TRCCEN,TRCRNF,
 CVK TRACING FOR DEBUGGING, V.Kotov:  not in use in present EIRENE version
      .                  TRCDBG2,TRCDBGE,TRCDBGM,TRCDBGF,TRCDBGL,
-     .                  TRCDBGS,TRCDBGG,TRCDBGMPI,TRCDBGC
+     .                  TRCDBGS,TRCDBGG,TRCDBGMPI,TRCDBGC,TRCHKTIM
       READ (IUNIN,6665) (TRCSRC(J),J=0,NSTRA)
 C
       READ (IUNIN,6666) NVOLPR, NSPCPR
@@ -3727,8 +3726,11 @@ C
 c    reset clock of source particles from old census to time0
 cdr  must be done also for time0=0.0, for otherwise flight time =0 is possible
 cdr  for census source particles and resulting error exits
+C Would gain performance by turning RPSTT into a pointer
             DO I=1,IPRNL
-              RPARTC(10,I)=TIME0
+              RPSTT(1:NPARTT)=RPARTC(1:NPARTT,I)
+              TIME=TIME0
+              RPARTC(1:NPARTT,I)=RPSTT(1:NPARTT)
             ENDDO
             WRITE (iunout,*) 'PARTICLE CLOCK RESET '
             WRITE (iunout,*) 'FIRST TIMESTEP RUNS FROM TIM1 TO TIM2:  '
@@ -5121,19 +5123,19 @@ c  number of spectra directly estimated from Monte-Carlo trajectories
 
       DO J = 1, NADSPC
 !  directional spectrum in geometrical cell
-        IF (ESTIML(J)%PSPC%ISRFCLL == 2) THEN
-          ISPZ=IADTYP(ESTIML(J)%PSPC%IPRTYP) + ESTIML(J)%PSPC%IPRSP
+        IF (ESTIML(J)%ISRFCLL == 2) THEN
+          ISPZ=IADTYP(ESTIML(J)%IPRTYP) + ESTIML(J)%IPRSP
           NBACK_SPEC = NBACK_SPEC + COUNT(ISPZ_BACK(ISPZ,:)>0)
-          LSPCCLL(ESTIML(J)%PSPC%ISPCSRF) = .TRUE.
+          LSPCCLL(ESTIML(J)%ISPCSRF) = .TRUE.
         END IF
 
-        IF (ESTIML(J)%PSPC%ISRFCLL == 0) THEN
+        IF (ESTIML(J)%ISRFCLL == 0) THEN
 C  COUNT SURFACE SPECTRA
           NADSPC_S=NADSPC_S+1
-        ELSEIF (ESTIML(J)%PSPC%ISRFCLL == 1) THEN
+        ELSEIF (ESTIML(J)%ISRFCLL == 1) THEN
 C  COUNT CELL-BASED SPECTRA
           NADSPC_C=NADSPC_C+1
-        ELSEIF (ESTIML(J)%PSPC%ISRFCLL == 2) THEN
+        ELSEIF (ESTIML(J)%ISRFCLL == 2) THEN
 C  COUNT DIRECTIONAL CELL-BASED SPECTRA
           NADSPC_D=NADSPC_D+1
         ENDIF
