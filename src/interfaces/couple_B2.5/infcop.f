ace5aadb6d70128300737a4cae6a85fa67bda49e
Michael Rack <m.rack@fz-juelich.de>
2018-04-20 12:09:16 +0200

    Adjusted name of new interface routine before mcarlo call. Added another interface routine within strata-loop that is called after every strata.

--- src/interfaces/couple_B2.5/infcop.f
+++ src/interfaces/couple_B2.5/infcop.f
@@ -2849,7 +2849,25 @@ C
 C> \brief Any property requirering hand-over in parallel part.
 C>
 C> This interfacing routine is called in the parallel part of EIRENE
-C> after the broadcase of any other quantity.
-      SUBROUTINE EIRENE_IFPARCOP
+C> after the broadcase of any other quantity and before MCARLO.
+      SUBROUTINE EIRENE_INFCOP_PRE_MCARLO
       RETURN
-      END SUBROUTINE EIRENE_IFPARCOP
+      END SUBROUTINE EIRENE_INFCOP_PRE_MCARLO
+
+C> \brief Return data at the end of an EIRENE stratum
+C>
+C> At this entry data are transferred back from EIRENE to the
+C> interfacing module (and from there, after possibly further
+C> processing, to the external code). This entry is called from the
+C> "strata-loop" in subr. MCARLO, after all trajectories for a
+C> particular stratum ISTRA have been sampled and after all volume and
+C> surface tallies have been scaled and processed to their final form.
+C>
+C> The call to IF3COP is controlled by the flag NMODE (input block 1).
+C> At call data are expected for stratum ISTRA. There they may be
+C> further prepared (e.g. normalized, or scaled to other units) for
+C> transfer to the external code
+      SUBROUTINE EIRENE_INFCOP_POST_STRATUM(ISTRA)
+      RETURN
+      END SUBROUTINE EIRENE_INFCOP_POST_STRATUM
+
