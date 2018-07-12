90fa7f44eb402ea418398bc85a548d762567f53f
Michael Rack <m.rack@fz-juelich.de>
2018-07-12 12:10:17 +0200

    Clean-up of moved strata deactivation and bugfix in locat0.

--- src/main-routines/mcarlo.f
+++ src/main-routines/mcarlo.f
@@ -280,20 +280,18 @@ C  CHANGED:  use XX=NTCPU seconds of cpu-time for calculation of trajectories
      .                     ' TURNED OFF, BECAUSE FLUX=0.0'
           CALL EIRENE_LEER(1)
         ENDIF
-        IF (NPTS(ISTRA).LE.0.OR.FLUX(ISTRA).LE.0.D0)
-     .     NLSRON(ISTRA) = .FALSE.
-        SUMM=0.
-        DO 2 ISRFS=1,NSRFSI(ISTRA)
-2         SUMM=SUMM+SORWGT(ISRFS,ISTRA)
-        IF (SUMM.LE.0.D0) THEN
-          WRITE (iunout,*) 'NO SOURCE MODEL FOR STRATUM NO ISTRA=',ISTRA
-          WRITE (iunout,*) 'BECAUSE THE SUM OF THE FLUXES'
-          WRITE (iunout,*) 'FROM THE SUBSTRATA DEFINED BY'
-          WRITE (iunout,*) 'SORWGT(SUBSTRATUM,STRATUM) IS .LE. ZERO'
-          WRITE (iunout,*) 'THIS STRATUM IS TURNED OF !!'
+        IF (SUM(SORWGT(1:NSRFSI(ISTRA),ISTRA)).LE.0.D0) THEN
           NPTS(ISTRA)=0
           NLSRON(ISTRA)=.FALSE.
+          WRITE (iunout,*) 'STRATUM ISTRA= ',ISTRA,
+     .                     ' TURNED OFF, BECAUSE'
+          WRITE (iunout,*) 'THE SUM OF THE FLUXES'
+          WRITE (iunout,*) 'FROM THE SUBSTRATA DEFINED BY'
+          WRITE (iunout,*) 'SORWGT(SUBSTRATUM,STRATUM) IS .LE. ZERO'
+          CALL EIRENE_LEER(1)
         ENDIF
+        IF (NPTS(ISTRA).LE.0.OR.FLUX(ISTRA).LE.0.D0)
+     .     NLSRON(ISTRA) = .FALSE.
         XPT=XPT+FLOAT(NPTS(ISTRA))
         XFL=XFL+FLUX(ISTRA)
 7     CONTINUE
