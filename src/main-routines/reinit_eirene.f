f2e7294b7c2a04389719ad8510abe23b79c8aeb7
D.Reiter <d.reiter@fz-juelich.de>
2018-07-31 18:09:35 +0200

    remove redundant routine emis_profiles. Now superseeded by new emis-line options

--- src/main-routines/reinit_eirene.f
+++ src/main-routines/reinit_eirene.f
@@ -41,7 +41,6 @@ csw 18apr07
       call EIRENE_SPUTER_REINIT
 
       call EIRENE_BA_ALPHA_REINIT
-      call EIRENE_EMIS_PROFILES_REINIT
 !pb   call EIRENE_BA_GAMMA_REINIT
 !pb   call EIRENE_LY_BETA_REINIT
       call EIRENE_LININT_REINIT  !cdr, july 17, added
