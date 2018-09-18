d3ccfb3e972f1587bf434bd704f3aa171dbd22b9
D.Reiter <d.reiter@fz-juelich.de>
2018-09-18 16:53:39 +0200

    nothing

--- src/main-routines/eirene.f
+++ src/main-routines/eirene.f
@@ -465,7 +465,7 @@ C  HENCE: RESET IITER TO 1
 
 C  PRINT OUTPUT FOR IDL BASED EXTERNAL GRAPHICS AND POST PROCESSING
       IF (PLIDL.AND.(MY_PE == 0)) THEN
-         CALL EIRENE_MASBOX
+        CALL EIRENE_MASBOX
      .          ('OUTPUT OPTION: IDL, PER STRATUM ')
         call eirene_outidlconf
         call eirene_outidlsrf
