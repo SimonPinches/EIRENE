1d1ead291334d4166dad30d0e69272e637f34d32
D.Reiter <d.reiter@fz-juelich.de>
2018-09-20 13:45:47 +0200

    nullify crm in read_photdbk activated again. Apparently this was not the reason for regression test failure

--- src/file-handling/read_photdbk.f
+++ src/file-handling/read_photdbk.f
@@ -320,7 +320,7 @@ c  reaction no IR is a "photonic" reaction
       nullify (reacdat(ir)%phr%adas)
       nullify (reacdat(ir)%phr%poly)
       nullify (reacdat(ir)%phr%hyd)
-ctest nullify (reacdat(ir)%phr%crm)
+      nullify (reacdat(ir)%phr%crm)
 
       reacdat(ir)%phr%line => phline
       reacdat(ir)%phr%ifit = -1
