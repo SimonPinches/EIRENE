b1f974415c9c978c38eed4e9eaea3d3de0f7d1cd
D.Reiter <d.reiter@fz-juelich.de>
2018-10-05 15:18:20 +0200

    comment only

--- src/startup-routines/settxt.f
+++ src/startup-routines/settxt.f
@@ -6,7 +6,9 @@ cdr  dec. 15 : energy source tallies for bulk ions: additional species index ipl
 cdr            tallies 38,44,50,56 and 84
 cdr  dec.17:   pumped flux tally SPUMP:  range 1--N5=NSPZ,  rather than N7+1--N8
 cdr            size of array LMETSPW decreased accordingly
-cdr  june 18:  nlemis used to condition some storage setting (FOR REVISED BLOCK 12) 
+cdr  june 18:  nlemis used to condition some storage setting (FOR REVISED BLOCK 12)
+cdr  oct 18 :  setting text and range for input tallies: moved to own routines:
+cdr            settxt_intal, and setprm_intal, to accomodate also input gradient tallies. 
 
       SUBROUTINE EIRENE_SETTXT
 c  Set default texts  (volume tallies: name, species, units), 
