a3183144467bc20b5bb9454cf6a7e226d16a4ec3
D.Reiter <d.reiter@fz-juelich.de>
2019-02-22 14:32:35 +0100

    remove old Balmer  and Lyman emissivity routines. Now under "snippets"

--- src/output/outpla.f
+++ src/output/outpla.f
@@ -146,6 +146,7 @@ c  positive tally numbers ital: output tallies, printed from OUTEIR.
             NFTE=MAX(NFTI,NSPEZV(IPRV,2))
           ENDIF
           DO 119 K=NFTI,NFTE
+c  check for valid range of tally ITALI
             IF (K.GT.NFSTPI(ITALI)) THEN
               CALL EIRENE_LEER(1)
               WRITE (iunout,*)
@@ -154,6 +155,7 @@ c  positive tally numbers ital: output tallies, printed from OUTEIR.
               CALL EIRENE_LEER(1)
               GOTO 119
             ENDIF
+
             SELECT CASE (ITALI)
             CASE (1)
               HELPP(1:NSBOX) = TEIN(1:NSBOX)
