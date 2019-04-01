SimonPinches/EIRENE

commit eef877c08efc04f9b2db1766cbd209ffb59e3f02

connecting new line emission options into diagno module, from branch emiss.

src/broadcast/broadcast.f
@@ -38,6 +38,9 @@ c    Aug. 17:  NMODE, LSMOPRO: exception wrt. MPI.  Why necessary?
 c              broadcasting of CHRTLS was done twice.  removed once.
 cpb  Dec. 17:  remove type SPECT_ARRAY, not needed in Fortran 2003
 c    Jan. 18:  new submodule alloc_fit_form used to allocate, and initialize REACDAT(IR) 
+cdr  May 18 :  broadcast new variables for internal CR code (currently H_COLRAD):
+cdr            nhcol_store
+cdr            m_hcol(nreac)
 
       SUBROUTINE EIRENE_BROADCAST
 cdr 
@@ -67,6 +70,7 @@ cdr
       USE EIRMOD_CTETRA
       USE EIRMOD_COMPRT
       USE EIRMOD_CPES
+      USE EIRMOD_COMNNL
       USE EIRMOD_COMSOU
       USE EIRMOD_CSTEP
       USE EIRMOD_COMSPL
@@ -228,19 +232,44 @@ cdr:  LSMOPRO, NMODE:  what is special about them to require treatment as except
 
       CALL MPI_BCAST (LLOGAU,NLOGAU,MPI_LOGICAL,0,MPI_COMM_WORLD,ier)
 
+      CALL MPI_BCAST (DTIMV,1,MPI_REAL8,0,MPI_COMM_WORLD,ier)
+      CALL MPI_BCAST (DTIMVI,1,MPI_REAL8,0,MPI_COMM_WORLD,ier)
+      CALL MPI_BCAST (DTIMVN,1,MPI_REAL8,0,MPI_COMM_WORLD,ier)
+      CALL MPI_BCAST (TIME0,1,MPI_REAL8,0,MPI_COMM_WORLD,ier)
+      CALL MPI_BCAST (RPART,NPRNL*NPARTT,MPI_REAL8,
+     .                0,MPI_COMM_WORLD,ier)
+      CALL MPI_BCAST (RPARTC,NPRNL*NPARTT,MPI_REAL8,
+     .                0,MPI_COMM_WORLD,ier)
+      CALL MPI_BCAST (RPARTW,NPRNL+1,MPI_REAL8,0,MPI_COMM_WORLD,ier)
+      CALL MPI_BCAST (NPRNLI,1,MPI_INTEGER,0,MPI_COMM_WORLD,ier)
+      CALL MPI_BCAST (IPRNLI,1,MPI_INTEGER,0,MPI_COMM_WORLD,ier)
+      CALL MPI_BCAST (IPRNLS,1,MPI_INTEGER,0,MPI_COMM_WORLD,ier)
+      CALL MPI_BCAST (IPRNL ,1,MPI_INTEGER,0,MPI_COMM_WORLD,ier)
+      CALL MPI_BCAST (NPTST ,1,MPI_INTEGER,0,MPI_COMM_WORLD,ier)
+      CALL MPI_BCAST (NTMSTP,1,MPI_INTEGER,0,MPI_COMM_WORLD,ier)
+      CALL MPI_BCAST (ITMSTP,1,MPI_INTEGER,0,MPI_COMM_WORLD,ier)
+      CALL MPI_BCAST (IPART,NPRNL*MPARTT,MPI_INTEGER,
+     .                0,MPI_COMM_WORLD,ier)
+      CALL MPI_BCAST (IPARTC,NPRNL*MPARTT,MPI_INTEGER,
+     .                0,MPI_COMM_WORLD,ier)
+      CALL MPI_BCAST (NPRNLS,NSTRA,MPI_INTEGER,0,MPI_COMM_WORLD,ier)
+
       IF (ALLOCATED(RCMSIG)) THEN
         CALL MPI_BCAST (RCMSIG,NCMSIG,MPI_REAL8,0,MPI_COMM_WORLD,ier)
         CALL MPI_BCAST (FUFFER,NCHOR*NCHEN,MPI_REAL8,
      .                  0,MPI_COMM_WORLD,ier)
         CALL MPI_BCAST (ENERGY,NCHEN,MPI_REAL8,0,MPI_COMM_WORLD,ier)
         CALL MPI_BCAST (ICMSIG,MCMSIG,MPI_INTEGER,0,MPI_COMM_WORLD,ier)
+        CALL MPI_BCAST (NLSTCHR,NCHOR,MPI_LOGICAL,0,MPI_COMM_WORLD,ier)
+        CALL MPI_BCAST (CH_LINE_NAME,80*NCHOR,MPI_CHARACTER,
+     .                   0,MPI_COMM_WORLD,ier)
       END IF
       CALL MPI_BCAST (NCHORI,1,MPI_INTEGER,0,MPI_COMM_WORLD,ier)      
       CALL MPI_BCAST (NCHENI,1,MPI_INTEGER,0,MPI_COMM_WORLD,ier)
 cdr  additional output tallies added by code itself (rather than via input block 14).
       CALL MPI_BCAST (MOD_ADDV,1,MPI_INTEGER,0,MPI_COMM_WORLD,ier)
 
-      IF (num_LINES > 0) THEN
+      IF (NUM_LINES > 0) THEN
         CALL EIRENE_BROAD_EMIS_LINES
       END IF
 
@@ -726,6 +755,11 @@ c  data for photon line transport
       CALL MPI_BCAST (IBGKPH,NPHOT*NREAC,MPI_INTEGER,
      .                0,MPI_COMM_WORLD,ier)
       CALL MPI_BCAST (NREACI,1,MPI_INTEGER,0,MPI_COMM_WORLD,ier)
+
+cdr something for the internal CRM options, of blocks 4,12 here: H_Colrad.
+      CALL MPI_BCAST (NHCOL_STORE,1,MPI_INTEGER,0,MPI_COMM_WORLD,ier)
+      CALL MPI_BCAST (M_HCOL,NREAC,MPI_INTEGER,0,MPI_COMM_WORLD,ier)
+
 
       CALL MPI_BCAST (NADDI,NTALV,MPI_INTEGER,0,MPI_COMM_WORLD,ier)
       CALL MPI_BCAST (NFRSTI,NTALV,MPI_INTEGER,0,MPI_COMM_WORLD,ier)
@@ -1645,13 +1679,13 @@ cdr     INVALID RP%IFIT
 
 !     IF (MY_PE /= 0) THEN
         IF (.NOT.ALLOCATED(EMIS_LINES)) THEN
-          ALLOCATE (EMIS_LINES(num_LINES))
+          ALLOCATE (EMIS_LINES(NUM_LINES))
           EMIS_LINES%LINE_NAME = REPEAT(' ',80)
           EMIS_LINES%NUM_COMPO = 0         
         END IF
 !     END IF
 
-      DO I = 1, num_LINES
+      DO I = 1, NUM_LINES
 
         CALL MPI_BCAST (EMIS_LINES(I)%LINE_NAME,80,MPI_CHARACTER,
      .                  0,MPI_COMM_WORLD,ier)

src/diagno/sgnal.f
@@ -13,7 +13,9 @@ cdr Oct 17  :
 cdr from W.Zholobenko: add         He emission lines, new options NCHTAL=5       
 cdr                    analogous to H emission lines,             NCHTAL=2 
 cdr       : added ITP (type of relevant component)
-cdr Jan 18: added MX_COMPO           
+cdr Jan 18: added MX_COMPO, generalize storage allocation PSIG(ND) for NCHTAL=2 option
+cdr         NCHTAL=5 option (He lines) is now redundant, due to generalization
+cdr                  of NCHTAL=2 option to arbitrary atomic or molecular lines.                   
 C
 C
       SUBROUTINE EIRENE_SGNAL(ICHORI,IISTR,ISP,ITP,LCHOR)
@@ -445,7 +447,24 @@ C
         ND = 0
 
         IF (ANY(NCHTAL == 1)) ND = MAX(ND, NATMI)
-        IF (ANY(NCHTAL == 2)) ND = MAX(ND,10)     
+
+        IF (ANY(NCHTAL == 2)) THEN
+c  how many components are requested (max) for line "ichori"?
+c  previous default: 6 hydrogenic lines, but only one per run. 
+c                    6 components each, (H, H+, H2, H2+ H-, H3+)
+          MX_COMPO = 0
+          IF (ALLOCATED(EMIS_LINES)) THEN
+            DO I=1, NUM_LINES
+              MX_COMPO = MAX(MX_COMPO, EMIS_LINES(I)%NUM_COMPO)
+            END DO
+          ELSE
+            write (iunout,*) 'error in diagno/sgnal: '
+            write (iunout,*) 'no emission data storage allocated'
+            CALL EIRENE_EXIT_OWN(1)
+          END IF
+          ND = MAX(ND, MX_COMPO)
+        END IF
+
         IF (ANY(NCHTAL == 3)) ND = MAX(ND, NPHOTI)
 
         IF (ANY(NCHTAL == 10)) ND = MAX(ND, NSPZ)
@@ -455,9 +474,10 @@ C
       END IF
 
       IF (NCHTAL(ICHORI).EQ.1)  NSPI=NATMI  ! post collision CX atomic species
-      IF (NCHTAL(ICHORI).EQ.2)  NSPI=10     ! up to 10 spectral line emissivities (transitions) in one single LOS evaluation
+cdr   IF (NCHTAL(ICHORI).EQ.2)  NSPI=10  ! THIS OPTION WAS FOR H EMISSION LINES. Now superseeded.
+      IF (NCHTAL(ICHORI).EQ.2)  NSPI=MX_COMPO ! use maximum number of components to spectral line emissivities (transitions) in one single LOS evaluation
       IF (NCHTAL(ICHORI).EQ.3)  NSPI=NPHOTI ! one spectrally resolved radiance per LOS and per photon species ("transition")
-      IF (NCHTAL(ICHORI).EQ.5)  NSPI=10  
+      IF (NCHTAL(ICHORI).EQ.5)  NSPI=10 ! THIS OPTION WAS FOR HE EMISSION LINES. Now superseeded.
       IF (NCHTAL(ICHORI).EQ.10) NSPI=NSPZ   ! 3rd party specified LOS integrals.
       PSIG = 0._DP
       IFIRST=0

src/diagno/sigha.f
@@ -13,12 +13,15 @@ cdr Jan 18:  parameter ICHORI added
 c            
 C
       SUBROUTINE EIRENE_SIGHA(INIT,JJJ,ZDS,PEN,PSIG,DUMMY2,ARGST,ICHORI)
-CDR  this routine evaluates ("side on") hydrogen atom ("HA") emissivities,
+CDR  this routine evaluates ("side on") emissivities of certain transition lines,
 cdr  integrated along a line of side (PSIG) and also the integrant resolved along 
 cdr  line of side (ARGST).
-c    Currently there are up to 6 contributions to each particular preprogrammed
+cdr new version:  the lines, components and contributions are specified in input block 12.
+
+cdr old version (up to May 2018):
+c    Currently there are up to 6 contributions to each particular pre-programmed
 c    transition (depending on population coefficient data stored 
-c    in file AMJUEL, section H.11 and H.12 
+c    in file AMJUEL, section H.11 and H.12) 
 c  aug.16: available transitions in H-atom:
 c          ly-alpha  (2 - 1)
 c          ly-beta   (3 - 1)
@@ -26,14 +29,16 @@ c          ba-alpha  (3 - 2)
 c          ba-beta   (4 - 2)
 c          ba-gamma  (5 - 2)
 c          ba-delta  (6 - 2)
-c    for each of these lines there are separate contributions from
+c    for each of these lines there are separate components from
 c    1) coupling to H
 c    2) coupling to H+
 c    3) coupling to H2
 c    4) coupling to H2+
 c    5) coupling to H-
 c    6) coupling to H3+
-c    0) total, sum over these 6 contributions
+c    0) total, sum over these 6 components.
+c    for each of this components there may be several contributions,
+c    e.g. component 1) may have contributions from H, D and T atoms.
 c
 c
 C
@@ -43,9 +48,9 @@ C          NCELL (COMPRT): INDEX IN TALLY ARRAYS FOR CURRENT ZONE
 C          JJJ:    INDEX OF SEGMENT ALONG CHORD
 C          ZDS:    LENGTH OF SEGMENT NO. JJJ
 C          PEN:    CENTRAL ENERGY OF LINE (EV)
-C  OUTPUT: PSIG:  LINE INTEGRAL OF EMISSION,I=0,6 CONTRIBUTIONS
+C  OUTPUT: PSIG:  LINE INTEGRAL OF EMISSION,I=0,6 COMPONENTS
 C          ARGST: CONTRIB. FROM CELL NCELL AND CHORD SEGMENT JJJ TO:
-C          THE H LINE FLUX PSIG(I),I=0,6 CONTRIBUTIONS
+C          THE H LINE FLUX PSIG(I),I=0,6 COMPONENTS
 C          FROM ATOMS, MOLECULES, TEST IONS, BULK IONS AND NEGATIV IONS
 C          THE INTEGRANT ARGST IS SUCH THAT INTEGR.(ARGST*DL) = PSIG
 C
@@ -64,6 +69,8 @@ C
       REAL(DP), INTENT(IN) :: ZDS, DUMMY2, PEN
       REAL(DP), INTENT(IN OUT) :: PSIG(0:), ARGST(0:,:)
       REAL(DP) :: PENOLD
+      INTEGER, SAVE :: LNO
+      INTEGER :: JCOMP, IADV
       INTEGER :: ISTOLD, ISP, NCELC, ICELL, ITROLD
       LOGICAL :: LARGST
       DATA ISTOLD/-1/
@@ -85,48 +92,12 @@ C  INITIALISE ATOMIC H-LINE ARRAYS FOR CURRENT STRATUM ?
         IF ((ISTRA .NE. ISTOLD) .OR. (IITER .NE. ITROLD) .OR.
      .      (PEN .NE. PENOLD) ) then
 c  new, unified routine for line emissivities, replacing: Ly_alpha, Ba_alpha, Ba_beta, etc.
-!         CALL EIRENE_EMIS_PROFILES (ISTRA,PEN,
-!    .                 NADVI+1,NADVI+2,NADVI+3,NADVI+4,NADVI+5,NADVI+6,
-!    .                 NADVI+7)
 
-          if (PEN.EQ.12.089_DP) THEN
-            write (iunout,*) ' ly_beta '
-            CALL EIRENE_Ly_beta 
-     .          (ISTRA,NADVI+1,NADVI+2,NADVI+3,NADVI+4,NADVI+5,NADVI+6,
-     .                 NADVI+7)
-          elseif (PEN.EQ.10.2375_DP) THEN
-            write (iunout,*) ' ly_alpha '
-            CALL EIRENE_Ly_alpha 
-     .          (ISTRA,NADVI+1,NADVI+2,NADVI+3,NADVI+4,NADVI+5,NADVI+6,
-     .                 NADVI+7)
-          elseif (PEN.EQ.3.0222_DP) THEN
-            write (iunout,*) ' ba_delta '
-            CALL EIRENE_Ba_delta
-     .          (ISTRA,NADVI+1,NADVI+2,NADVI+3,NADVI+4,NADVI+5,NADVI+6,
-     .                 NADVI+7)
-           elseif (PEN.EQ.2.8560_DP) THEN
-            write (iunout,*) ' ba_gamma '
-            CALL EIRENE_Ba_gamma
-     .          (ISTRA,NADVI+1,NADVI+2,NADVI+3,NADVI+4,NADVI+5,NADVI+6,
-     .                 NADVI+7)
-          elseif (PEN.EQ.2.5500_DP) THEN
-            write (iunout,*) ' ba_beta '
-            CALL EIRENE_Ba_beta
-     .          (ISTRA,NADVI+1,NADVI+2,NADVI+3,NADVI+4,NADVI+5,NADVI+6,
-     .                 NADVI+7)
-          elseif (PEN.EQ.1.8889_DP) THEN 
-            write (iunout,*) ' ba_alpha '
-            CALL EIRENE_Ba_alpha
-     .          (ISTRA,NADVI+1,NADVI+2,NADVI+3,NADVI+4,NADVI+5,NADVI+6,
-     .                 NADVI+7)
-          else
-            WRITE (IUNOUT,*) 'NO LINE DEFINITION FOUND FOR PEN=',PEN
-            WRITE (IUNOUT,*) 'SIGNAL IS SET TO 0'
-            ADDV(NADVI+1:NADVI+7,:) = 0._DP
-          endif
+c  identify the selected emission line LNO from the input flags.
+          CALL EIRENE_FIND_EMIS_LINE (ISTRA,ICHORI,PEN,LNO)
 
 
-        endif   ! NEW INTERNAL ITERATION, OR NEW LINE, OR NEW STRATUM
+        endif   ! additional tallies ADDV are now filled, for new LINE, and for present stratum
 
         ISTOLD=ISTRA
         ITROLD=IITER
@@ -138,22 +109,24 @@ C  LINE INTEGRAL: PHOTONS/SEC/CM**2
 C
 C
       ncelc=ncltal(ncell)
-      PSIG(1)=PSIG(1)+ZDS*ADDV(NADVI+1,NCELC)
-      PSIG(2)=PSIG(2)+ZDS*ADDV(NADVI+2,NCELC)
-      PSIG(3)=PSIG(3)+ZDS*ADDV(NADVI+3,NCELC)
-      PSIG(4)=PSIG(4)+ZDS*ADDV(NADVI+4,NCELC)
-      PSIG(5)=PSIG(5)+ZDS*ADDV(NADVI+5,NCELC)
-      PSIG(6)=PSIG(6)+ZDS*ADDV(NADVI+6,NCELC)
-      PSIG(0)=PSIG(0)+ZDS*ADDV(NADVI+7,NCELC)
 
-      IF (LARGST) THEN
-        ARGST(1,JJJ)=ADDV(NADVI+1,NCELC)
-        ARGST(2,JJJ)=ADDV(NADVI+2,NCELC)
-        ARGST(3,JJJ)=ADDV(NADVI+3,NCELC)
-        ARGST(4,JJJ)=ADDV(NADVI+4,NCELC)
-        ARGST(5,JJJ)=ADDV(NADVI+5,NCELC)
-        ARGST(6,JJJ)=ADDV(NADVI+6,NCELC)
-        ARGST(0,JJJ)=ADDV(NADVI+7,NCELC)
+      IF (LNO == 0) THEN
+! NO MATCHING EMISSION LINE FOUND 
+        PSIG(0) = 0._DP
+        IF (LARGST) ARGST(0,JJJ) = 0._DP
+      ELSE
+! USE DATA PROVIDED FOR EMISSION LINE LNO
+        DO JCOMP = 1, EMIS_LINES(LNO)%NUM_COMPO
+          IADV = EMIS_LINES(LNO)%COMPO(JCOMP)%IADV
+          PSIG(JCOMP) = PSIG(JCOMP) + ZDS*ADDV(IADV,NCELC)
+          IF (LARGST) ARGST(JCOMP,JJJ) = ADDV(IADV,NCELC)
+        END DO
+
+c  sum over components of line LNO
+        IADV = EMIS_LINES(LNO)%IADV_TOTAL
+
+        PSIG(0) = PSIG(0) + ZDS*ADDV(IADV,NCELC)
+        IF (LARGST) ARGST(0,JJJ) = ADDV(IADV,NCELC)
       END IF
 C
       RETURN

src/interfaces/couple_B2/infcop.f
@@ -1838,8 +1838,8 @@ C
           IF (ORI(ITARG,IG).GT.0) NSEW='E'
         ENDIF
         IF (IGSTEP(ITARG,IG).LT.200000) THEN
-          IF (ORI(ITARG,IG).LT.0) NSEW='N'
-          IF (ORI(ITARG,IG).GT.0) NSEW='S'
+          IF (ORI(ITARG,IG).LT.0) NSEW='S'
+          IF (ORI(ITARG,IG).GT.0) NSEW='N'
         ENDIF
         WRITE (iunout,'(1X,I3,1P,9E11.3,3X,A1)')
      .             IG,RRSTEP(ITARG,IG),FLSTEP(0,ITARG,IG),

src/modules/eirmod_coutau.f
@@ -47,7 +47,7 @@ cdr dec. 15:  comments added. missing tallies ppeli, epeli, etc..??
      R VZDENAI(:,:), VZDENMI(:,:), VZDENII(:,:), VZDENPHI(:,:),
      R MAPLI(:,:),  MMPLI(:,:),  MIPLI(:,:),  MPHPLI(:,:)
  
-! SURFACE TALLIES
+! INTEGRALS OF SURFACE TALLIES
       REAL(DP), PUBLIC, ALLOCATABLE, SAVE ::
      R POTATI(:,:), PRFAAI(:,:), PRFMAI(:,:), PRFIAI(:,:), PRFPHAI(:,:),
      R PRFPAI(:,:),
@@ -67,7 +67,8 @@ cdr dec. 15:  comments added. missing tallies ppeli, epeli, etc..??
      R ERFPII(:,:),
      R EOTPHTI(:,:), ERFAPHTI(:,:), ERFMPHTI(:,:), ERFIPHTI(:,:),
      R ERFPHPHTI(:,:), ERFPPHTI(:,:),
-     R EOTPLI(:,:),
+     R EOTPLI(:,:)
+      REAL(DP), PUBLIC, ALLOCATABLE, SAVE ::
      R SPTAATI(:,:), SPTMATI(:,:), SPTIATI(:,:), SPTPHATI(:,:), 
      R SPTPATI(:,:),
      R SPTAMLI(:,:), SPTMMLI(:,:), SPTIMLI(:,:), SPTPHMLI(:,:), 

src/particle-tracing/samvol.f
@@ -816,41 +816,43 @@ C
           GOTO 999
         ENDIF
 C...................................................................  
-      case (3:4)
-        select case (LEVGEO)
-        case (3)
-          IF (.NOT.NLPOL) THEN
-            GOTO 999
-          ENDIF
-          IN = NRCELL + (NPCELL-1)*NR1ST
-          ZEP1=AREA(IN)*RANF_EIRENE()
-!pb          IF (ZEP1.LE.ASIMP(1,NCELL)) THEN
-          IF (ZEP1.LE.ASIMP(1,IN)) THEN
-C   PUNKT IN DREIECK 1
-            X1=XPOL(NRCELL,NPCELL)
-            X2=XPOL(NRCELL,NPCELL+1)
-            X3=XPOL(NRCELL+1,NPCELL+1)
-            Y1=YPOL(NRCELL,NPCELL)
-            Y2=YPOL(NRCELL,NPCELL+1)
-            Y3=YPOL(NRCELL+1,NPCELL+1)
-          ELSE
-C   PUNKT IN DREIECK 2
-            X1=XPOL(NRCELL+1,NPCELL)
-            X2=XPOL(NRCELL,NPCELL)
-            X3=XPOL(NRCELL+1,NPCELL+1)
-            Y1=YPOL(NRCELL+1,NPCELL)
-            Y2=YPOL(NRCELL,NPCELL)
-            Y3=YPOL(NRCELL+1,NPCELL+1)
-          ENDIF
-          IPOLG=NPCELL
-        case (4)
-          X1=XTRIAN(NECKE(1,NCELL))
-          X2=XTRIAN(NECKE(2,NCELL))
-          X3=XTRIAN(NECKE(3,NCELL))
-          Y1=YTRIAN(NECKE(1,NCELL))
-          Y2=YTRIAN(NECKE(2,NCELL))
-          Y3=YTRIAN(NECKE(3,NCELL))
-        end select
+      case (3)
+        IF (.NOT.NLPOL) THEN
+          GOTO 999
+        ENDIF
+        IN = NRCELL + (NPCELL-1)*NR1ST
+        ZEP1=AREA(IN)*RANF_EIRENE()
+        IF (ZEP1.LE.ASIMP(1,IN)) THEN
+C   POINT TO BE SAMPLED WITHIN TRIANGLE 1
+          X1=XPOL(NRCELL,NPCELL)
+          X2=XPOL(NRCELL,NPCELL+1)
+          X3=XPOL(NRCELL+1,NPCELL+1)
+          Y1=YPOL(NRCELL,NPCELL)
+          Y2=YPOL(NRCELL,NPCELL+1)
+          Y3=YPOL(NRCELL+1,NPCELL+1)
+        ELSE
+C   POINT TO BE SAMPLED WITHIN TRIANGLE 2
+          X1=XPOL(NRCELL+1,NPCELL)
+          X2=XPOL(NRCELL,NPCELL)
+          X3=XPOL(NRCELL+1,NPCELL+1)
+          Y1=YPOL(NRCELL+1,NPCELL)
+          Y2=YPOL(NRCELL,NPCELL)
+          Y3=YPOL(NRCELL+1,NPCELL+1)
+        ENDIF
+        IPOLG=NPCELL
+        Z1=0.
+        Z2=0.
+        Z3=0.
+        CALL EIRENE_FPOLYT_3(X1,Y1,Z1,X2,Y2,Z2,X3,Y3,Z3,X0,Y0,ZZ)
+
+C...................................................................  
+      case (4)
+        X1=XTRIAN(NECKE(1,NCELL))
+        X2=XTRIAN(NECKE(2,NCELL))
+        X3=XTRIAN(NECKE(3,NCELL))
+        Y1=YTRIAN(NECKE(1,NCELL))
+        Y2=YTRIAN(NECKE(2,NCELL))
+        Y3=YTRIAN(NECKE(3,NCELL))
         Z1=0.
         Z2=0.
         Z3=0.

commit 08247df309bc62c000b0980e08ccc64092e91c4a

Bug fix: wrong broadcast of census variables
Fixes bug which was introduced in eef877c0.
Broadcast of census variables takes place in separate subroutine EIRENE_BROAD_COMNNL.

src/broadcast/broadcast.f
@@ -72,7 +72,6 @@ cdr
       USE EIRMOD_CTETRA
       USE EIRMOD_COMPRT
       USE EIRMOD_CPES
-      USE EIRMOD_COMNNL
       USE EIRMOD_COMSOU
       USE EIRMOD_CSTEP
       USE EIRMOD_COMSPL
@@ -232,28 +231,6 @@ cdr:  LSMOPRO, NMODE:  what is special about them to require treatment as except
      .                MPI_INTEGER,0,MPI_COMM_WORLD,ier)
 
       CALL MPI_BCAST (LLOGAU,NLOGAU,MPI_LOGICAL,0,MPI_COMM_WORLD,ier)
-
-      CALL MPI_BCAST (DTIMV,1,MPI_REAL8,0,MPI_COMM_WORLD,ier)
-      CALL MPI_BCAST (DTIMVI,1,MPI_REAL8,0,MPI_COMM_WORLD,ier)
-      CALL MPI_BCAST (DTIMVN,1,MPI_REAL8,0,MPI_COMM_WORLD,ier)
-      CALL MPI_BCAST (TIME0,1,MPI_REAL8,0,MPI_COMM_WORLD,ier)
-      CALL MPI_BCAST (RPART,NPRNL*NPARTT,MPI_REAL8,
-     .                0,MPI_COMM_WORLD,ier)
-      CALL MPI_BCAST (RPARTC,NPRNL*NPARTT,MPI_REAL8,
-     .                0,MPI_COMM_WORLD,ier)
-      CALL MPI_BCAST (RPARTW,NPRNL+1,MPI_REAL8,0,MPI_COMM_WORLD,ier)
-      CALL MPI_BCAST (NPRNLI,1,MPI_INTEGER,0,MPI_COMM_WORLD,ier)
-      CALL MPI_BCAST (IPRNLI,1,MPI_INTEGER,0,MPI_COMM_WORLD,ier)
-      CALL MPI_BCAST (IPRNLS,1,MPI_INTEGER,0,MPI_COMM_WORLD,ier)
-      CALL MPI_BCAST (IPRNL ,1,MPI_INTEGER,0,MPI_COMM_WORLD,ier)
-      CALL MPI_BCAST (NPTST ,1,MPI_INTEGER,0,MPI_COMM_WORLD,ier)
-      CALL MPI_BCAST (NTMSTP,1,MPI_INTEGER,0,MPI_COMM_WORLD,ier)
-      CALL MPI_BCAST (ITMSTP,1,MPI_INTEGER,0,MPI_COMM_WORLD,ier)
-      CALL MPI_BCAST (IPART,NPRNL*MPARTT,MPI_INTEGER,
-     .                0,MPI_COMM_WORLD,ier)
-      CALL MPI_BCAST (IPARTC,NPRNL*MPARTT,MPI_INTEGER,
-     .                0,MPI_COMM_WORLD,ier)
-      CALL MPI_BCAST (NPRNLS,NSTRA,MPI_INTEGER,0,MPI_COMM_WORLD,ier)
 
       IF (ALLOCATED(RCMSIG)) THEN
         CALL MPI_BCAST (RCMSIG,NCMSIG,MPI_REAL8,0,MPI_COMM_WORLD,ier)