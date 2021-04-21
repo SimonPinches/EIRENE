4eaa907bb175b13ff199b55c0e709ea0c8af6d9b
Boerner, Petra <p.boerner@fz-juelich.de>
2021-04-21 08:51:48 +0000

    Update eirene.f for restarting CI

--- src/main-routines/eirene.f
+++ src/main-routines/eirene.f
@@ -1,6 +1,6 @@
-!pb  181206  output is done by processor 0
-!pb  181206  setting up of census source is done by processor 0
-!pb  100107  call to reinitialisation routine
+!PB  181206  output is done by processor 0
+!PB  181206  setting up of census source is done by processor 0
+!PB  100107  call to reinitialisation routine
 cdr  140416  allow for NSTRAI .le. NSTRA  (e.g. if time stratum has been turned off)
 cdr          currently turning off time stratum may not be detected
 cdr          when setting dynamic allocatable storage parameters in "find_param.f"
