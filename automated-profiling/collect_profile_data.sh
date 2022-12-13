d7d63a4321f137c7840a4257b8f32e34f5cc3868
Huw Leggate <huw.leggate@dcu.ie>
2022-12-13 18:38:43 +0200

    Added parsing of wall time

--- automated-profiling/collect_profile_data.sh
+++ automated-profiling/collect_profile_data.sh
@@ -16,25 +16,28 @@ do
         echo "---$case_name---"
 	cd $top_dir
 
-	#Check if we've already processed this case
 	profile_generated=$(read_report cases.${case_name}.profile_success)
-	if [ "$profile_generated" == "true" ];then
+	if [ "$profile_copied" == "true" ];then
 		echo "Already profiled"
 		continue
 	fi	
 
+        #Check if we've already parsed this case
+        profile_parsed=$(read_report cases.${case_name}.profile_parsed)
+        if [ "$profile_parsed" == "true" ];then
+                echo "Already parsed"
+                continue
+        fi      
+	   
 	#Check to see if the slurm job has finished
 	job_number=$(read_report cases.${case_name}.job_number |tr -d '"')
 	job_state=$(sacct -j $job_number --format jobid,state -n |sed -n -r "/^$job_number /s/($job_number| +)//gp")
-	report cases.${case_name}.job_state \"$job_state\"
-
 	while [ "$job_state" != "COMPLETED" ]; do	        
+		job_state=$(sacct -j $job_number --format jobid,state -n |sed -n -r "/^$job_number /s/($job_number| +)//gp")
 		echo "Job ${job_number} not complete yet, job state is ${job_state} ... Waiting ..."
 		sleep 10
-		job_number=$(read_report cases.${case_name}.job_number |tr -d '"')
-		job_state=$(sacct -j $job_number --format jobid,state -n |sed -n -r "/^$job_number /s/($job_number| +)//gp")
-		report cases.${case_name}.job_state \"$job_state\"
 	done
+	report cases.${case_name}.job_state \"$job_state\"
 
 	#Make a directory in the report dir for the case
 	case_report_dir=$scalability_report_dir/${sample}_profiles/$case_name
@@ -51,68 +54,13 @@ do
 	echo "Generating profile for slurm job ${job_number}"
 	echo "Copying job artifacts"
 	cp eirene-2d.reference_${N}-${n}-${c}.out slurm-${job_number}.out $case_report_dir/
-#	printf "Parsing gprof..."
-#	gprof ./eirene gmon_${N}-${n}-${c}.out > $case_report_dir/profile-${N}-${n}-${c}
-
-#	if [[ $? -eq 0 ]];then
-#		echo "Successfully generated profile ${sample}_profiles/$case_name/profile-${N}-${n}-${c}"
-#		report cases.${case_name}.profile_success "true"
-#	else
-#		echo "Failed to generate profile :("
-#		report cases.${case_name}.profile_success "false"
-#	fi
-done
-
-#TODO: this loop is grafter on here because I've ran the upper loop separately, they could be combined
-for case_name in $cases
-do
-	echo 
-        echo "---$case_name---"
-	cd $top_dir
-
-	#Check if we've already processed this case
-	profile_parsed=$(read_report cases.${case_name}.profile_parsed)
-	if [ "$profile_parsed" == "true" ];then
-		echo "Already parsed"
-		continue
-	fi	
-
-	#Check if we've generated the profile
-	profile_generated=$(read_report cases.${case_name}.profile_success)
-	if [ "$profile_generated" != "true" ];then
-		echo "Not profiled"
-		continue
-	fi	
-
-	#Make a directory in the report dir for the case
-	case_report_dir=$scalability_report_dir/${sample}_profiles/$case_name
-	mkdir -p $case_report_dir
-	cd $case_report_dir
-	
-	############################
-	# Parse gprof flat profile #
-	############################
-
-	report cases.${case_name}.flat_profile {}
-
-	N=$(read_report cases.${case_name}.n_nodes)
-	n=$(read_report cases.${case_name}.n_mpi_ranks)
-	c=$(read_report cases.${case_name}.n_omp_threads)
+	echo "Copied output ${sample}_profiles/$case_name/profile-${N}-${n}-${c}"
+	report cases.${case_name}.output_copied "true"
 
 	echo "Parsing output"
-#	sed -n -r '/^ *[0-9]/{p; :loop n; /^$/q; p; b loop}' ${case_report_dir}/profile-${N}-${n}-${c} | \
-#	while read pcent cumul self n_calls self_ms total_ms name
-#	do
-#		#Sometimes the last three numerical values are missing, so the name field is shifted "left"
-#		[ -z "$name" ] && name=$total_ms
-#		[ -z "$name" ] && name=$self_ms
-#		[ -z "$name" ] && name=$n_calls;n_calls=""
-#		if [[ "$self" != "0.00" ]]; then
-#			report cases.${case_name}.flat_profile.${name} {}
-#			report cases.${case_name}.flat_profile.${name}.time \"${self}\"
-#			report cases.${case_name}.flat_profile.${name}.percentage \"${pcent}\"
-#			report cases.${case_name}.flat_profile.${name}.n_calls \"${n_calls}\"
-#		fi
-#	done 
-
+	report cases.${case_name}.timing {}
+	cpu_time=$(grep CPU_TIME ${case_report_dir}/eirene-2d.reference_${N}-${n}-${c}.out | sed 's/[[:blank:]]*$//; s/.*[[:blank:]]//')
+	wall_time=$(echo ${cpu_time:0:8})
+	report cases.${case_name}.output_parsed "true"
+	report cases.${case_name}.timing.wall_time \"${wall_time}\"
 done
