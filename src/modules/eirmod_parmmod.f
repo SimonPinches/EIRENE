dd7b4f7cb3b3c931023712a2c7de9db6543ab0ef
Huw Leggate <huw.leggate@dcu.ie>
2023-05-16 20:08:42 +0100

    Set version back 1.0.1

--- src/modules/eirmod_parmmod.f
+++ src/modules/eirmod_parmmod.f
@@ -155,7 +155,7 @@ cdr  size of particle species columns in various arrays
       INTEGER, PUBLIC, SAVE :: INT_PARM(NUM_PARM)
 
       ! This value will be automatically replaced with the version number specified in version.txt
-      CHARACTER(12), PUBLIC, SAVE :: EIRENE_VERSION_STRING='1.0.1'
+      CHARACTER(12), PUBLIC, SAVE :: EIRENE_VERSION_STRING='1.0.2'
 
 
       PRIVATE :: EIRENE_SPEC_TO_SPEC
