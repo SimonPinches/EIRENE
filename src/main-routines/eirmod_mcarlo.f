c47e5f3be3d16f193eb88d5ccb5c4f310af348fc
Xavier Bonnin <Xavier.Bonnin@iter.org>
2023-11-22 11:04:27 +0100

    Adding tracking output to understand where CI fails

--- src/main-routines/eirmod_mcarlo.f
+++ src/main-routines/eirmod_mcarlo.f
@@ -1010,6 +1010,7 @@ c...................................................................
             ITRJ = NCHORI + MOD(IPANU,NTRJ) + 1
             NLEVEL=0
             CALL EIRENE_LOCAT1(IPANU)
+            WRITE(0,*) ' IN MCARLO, DOING PARTICLE : ', IPANU
 
 C  IS BIRTH PROCESS SURVIVED?
             IF (.NOT.LGPART) GOTO 110
