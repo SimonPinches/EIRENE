f2ccc65dd09d1cd2eda8735af73d3fb23de6ad51
Xavier Bonnin <Xavier.Bonnin@iter.org>
2024-10-28 09:53:42 +0100

    Removed unused IFOFF argument in EIRENE_FIND_TRIANG_DIM

--- src/output/outflx.f
+++ src/output/outflx.f
@@ -370,7 +370,7 @@ chk Legendre polynomial expansion tallies
                 IF (ESTIML(ISPC)%ISPCOPT==2) THEN
                   DO IE=1, ESTIML(ISPC)%NSPC
                     EN = ESTIML(ISPC)%SPCMIN +
-     .                   (IE-0.5)*ESTIML(ISPC)%SPCDEL
+     .                   (IE-0.5_DP)*ESTIML(ISPC)%SPCDEL
                     IF (ESTIML(ISPC)%LOG) THEN
                       WRITE (IOUT,'(I6,1ES12.4)') IE,10._DP**EN
                     ELSE
