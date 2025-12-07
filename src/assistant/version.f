756b094432238ce8d6061ac9bd290330a85791bc
Xavier Bonnin <Xavier.Bonnin@iter.org>
2025-12-07 11:18:26 +0100

    Second part of issue #1 resolution (now for standalone compilation outside SOLPS environment)

--- src/assistant/version.f
+++
@@ -1,5 +0,0 @@
-C> \brief Dummy for printing EIRENEs git version hash.
-C>
-C> Subroutine is filled via cmake with git version information.
-      SUBROUTINE EIRENE_VERSION
-      END SUBROUTINE EIRENE_VERSION
